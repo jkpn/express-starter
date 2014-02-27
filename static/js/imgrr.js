9523c1ce3ebf675a018f5ba306aa7043c19b010b
James Presley-Nelson <jamespresleynelson@gmail.com>
2014-02-26 20:17:20 -0800

    Completed part 7

--- static/js/imgrr.js
+++ static/js/imgrr.js
@@ -1,8 +1,21 @@
 // Problem 2 (Peekaboo) ------------------------------------------------------
-// WRITE CODE HERE
+
+$('#toggle_img').click(function() {
+	if ($('#toggle_img').text() === "Go Away!") {
+		$('#main_img').hide();
+		$('#toggle_img').text('Come Back!');
+	} else { 
+		$('#toggle_img').text('Go Away!');
+		$('#main_img').show(); 
+	}
+
+});
 
 // Problem 3 (Swap Em) -----------------------------------------------
-// WRITE CODE HERE
+$('#change_img').click(function() {
+	$('#main_img').attr('src', "/static/img/" + $('#new_img_file').val())
+	//alert ("/static/img/" + $('#new_img_file').val());
+});
 
 // Problem 4 (Find the Source) -------------------------------------------------
 $('.clickable').click(function() {
