edbfc49fdb57278aed2116bbb0e11ccdec2f418c
James Presley-Nelson <jamespresleynelson@gmail.com>
2014-04-02 19:46:17 -0700

    diagonal object

--- static/js/ball.js
+++ static/js/ball.js
@@ -6,22 +6,23 @@ $(document).ready(function() {
   var height = canvas.height;
 
   //PUT STUFF HERE
+  var ball1 = {x:20, y:20, radius:30, color:"red"}
   var x = 20;
+  var y = 20;
   //run an iteration of the game
   var updateGame = function() {
  
     context.fillStyle='blue';
     context.fillRect(0,0, 800, 800);
 
-    context.fillStyle='red';
+    context.fillStyle=ball1.color;
     context.beginPath();
-    context.arc(x,20,20,-Math.PI,Math.PI);
+    context.arc(ball1.x,ball1.y,ball1.radius,-Math.PI,Math.PI);
     context.closePath();
     context.fill();
 
- 
-
-     x = x + 5;
+     ball1.x = ball1.x + 5;
+     ball1.y = ball1.y + 5;
      setTimeout(updateGame, 10);
   };
 
