33b5ed91d1fbd9ec8c16926d87fb3827c6c6e433
James Presley-Nelson <jamespresleynelson@gmail.com>
2014-04-09 16:38:06 -0700

    for loop balls

--- static/js/chain_reaction.js
+++ static/js/chain_reaction.js
@@ -6,10 +6,41 @@ $(document).ready(function() {
   var height = canvas.height;
 
   // PUT STUFF HERE
+  var balls = []
+  
+  var b0 = {x:20, y:20, radius:10, color:"red", vx:5, vy:5}
+
+  var b1 = {x:45, y:45, radius:20, color:"black", vx:5, vy:5}
+
+  var b2 = {x:85, y:85, radius:30, color:"blue", vx:5, vy:5}
+   
+balls.push(b0)
+balls.push(b1)
+balls.push(b2)
+
 
   // Run an interation of the game
   var updateGame = function() {
-    // PUT STUFF HERE
+// PUT STUFF HERE
+for (var i = 0; i < balls.length; i++) {
+
+  context.fillStyle=balls[i].color;
+    context.beginPath();
+    context.arc(balls[i].x,balls[i].y,balls[i].radius,-Math.PI,Math.PI);
+    context.closePath();
+    context.fill();
+}
+    //context.fillStyle=b1.color;
+    //context.beginPath();
+    //context.arc(b1.x,b1.y,b1.radius,-Math.PI,Math.PI);
+    //context.closePath();
+    //context.fill();
+
+    //context.fillStyle=b2.color;
+    //context.beginPath();
+    //context.arc(b2.x,b2.y,b2.radius,-Math.PI,Math.PI);
+    //context.closePath();
+    //context.fill();
   };
 
   // Handle a canvas click event
